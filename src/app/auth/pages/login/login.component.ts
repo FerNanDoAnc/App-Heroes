6e32d991a12e06468b2c046febe70cfc762b6a55
Fernando Ancajima <fernando.ancajima99@gmail.com>
2021-12-04 14:57:03 -0500

    Auth service-mantener esaado de autenticacion

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,21 @@ import { Router } from '@angular/router';
 export class LoginComponent{
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
   
   ) { }
 
   login() {
     // Ir al backens  y validar que el usuario exista
-    this.router.navigate(['./heroes']);
+    this.authService.login()
+    .subscribe(resp => {
+      if (resp.id) {
+        this.router.navigate(['./heroes']);
+      }
+    });
+
+    // this.router.navigate(['./heroes']);
   }
 
 }
